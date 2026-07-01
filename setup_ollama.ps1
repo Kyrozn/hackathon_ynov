bdffedb5688fcc617fb869625e8bd6c0db13cd13
Kyrozn <kyrian.delaplace09@gmail.com>
2026-07-01 18:10:15 +0200

    [fix] setup ollama cp file  + modelfile

--- setup_ollama.ps1
+++ setup_ollama.ps1
@@ -8,8 +8,8 @@ Write-Host "Téléchargement du modèle phi3.5..."
 docker exec ollama ollama pull phi3.5
 
 Write-Host "Création du modèle personnalisé phi3.5-financial..."
-docker cp /models/phi3.5_financial/Modelfile ollama:/Modelfile_simple
-docker exec ollama ollama create phi3.5-financial -f /Modelfile_simple
+docker cp ./models/phi3.5_financial/Modelfile ollama:/Modelfile
+docker exec ollama ollama create phi3.5-financial -f /Modelfile
 
 Write-Host "Modèle phi3.5-financial créé avec succès."
 Write-Host "Note: Le modèle phi3.5 a été configuré avec un system prompt financier spécialisé."
